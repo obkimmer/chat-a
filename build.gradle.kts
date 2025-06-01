c954779bed1ee20094cebf47b50c5e9a3ad0c692
obkimmer <obkimmer@gmail.com>
2025-05-31 21:47:07 -0700

    Update build.gradle.kts
    changed the # comment to //

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-# build.gradle.kts (root)
+// build.gradle.kts (root)
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
