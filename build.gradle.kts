09be419aba8bf4414451e03d247034fb103fe136
obkimmer <obkimmer@gmail.com>
2025-05-31 22:44:34 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
+        google() // This must come before mavenCentral for AGP
+        mavenCentral()
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
 }
